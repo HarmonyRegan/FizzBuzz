3e298fbb60f75e25893eab1fe68f3b50d0c4f503
HarmonyRegan <119048336+HarmonyRegan@users.noreply.github.com>
2023-02-17 14:38:29 +0000

    check through numbers 1-20

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -10,19 +10,31 @@ namespace FizzBuzzKata
     {
         public string Check(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0)
-            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
                 return "FizzBuzz";
-            }
-            if (number % 3 == 0)
-            {
+                }
+                if (number % 3 == 0)
+                {
                 return "Fizz";
-            }
-            if(number % 5 == 0)
-            {
+                }
+                if(number % 5 == 0)
+                {
                 return "Buzz";
-            }
+                }
             return number.ToString();
         }
-    }
-}
+
+        public string[] Check(int[] input)
+            
+        {
+            var results = new List<string>();
+            for (int number = 0; number < input.Count(); number++)
+            {
+                results.Add(Check(input[number]));
+            }
+            return results.ToArray();
+        }
+     }
+};
+    
--- FizzBuzzKata/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzTest.cs
@@ -8,9 +8,16 @@ namespace FizzBuzzKata
         [InlineData(5, "Buzz")]
         [InlineData(15, "FizzBuzz")]
         public void FizzBuzzTests(int input, string expected)
-        {
+        { 
             var number = new FizzBuzz().Check(input);
             Assert.Equal(expected, number);
+        }
+        [Fact]
+        public void Check()
+        {
+            var input = Enumerable.Range(1, 20).ToArray();
+            var actual = new FizzBuzz().Check(input);
+            Assert.Equal(new[] { "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz" }, actual);
         }
     } 
 }
